347c3aa0c3bba2a1f065ce6b13966d0b273de7c5
정윤아 <yajung0203@naver.com>
2021-05-29 16:33:08 +0900

    유튜브 소개 완료

--- js/views/introduce/youtube-intro.js
+++ js/views/introduce/youtube-intro.js
@@ -1,2 +1,72 @@
 "use strict";
-console.log("youtube-intro");
+/*
+    Copyright © 2021 지구방위대.
+    ProjectName: saveEarthWebPJ
+    FilePath: js/views/introduce/youtube-intro.js
+    Create by 지구방위대, 정윤아 on 2021-05-29 16:01:48.
+*/
+
+(function ($) {
+  const $appBodyEl = $(".app-body");
+  const $fullPageEl = $("#fullPage");
+  AOS.init();
+
+  $appBodyEl.css("overflow", "hidden");
+
+  // 스크롤 설정
+  $fullPageEl.fullpage({
+    // sectionsColor: ["#4BBFC3", "#7BAABE", "whitesmoke", "#000"],
+    sectionSelector: ".section",
+    anchors: ["first", "second", "third", "forth"],
+    navigation: false,
+    lockAnchors: true,
+    verticalCentered: true,
+    resize: false,
+    scrollOverflow: true,
+    responsive: 921,
+    //Scrolling
+    css3: true,
+    scrollBar: true,
+    autoScrolling: true,
+    fixedElements: ".app-header",
+    afterResize: function () {
+      AOS.init(); // AOS initiation
+      $(".aos-init").removeClass("aos-animate");
+
+      const a_table = ["first", "second", "third", "forth"]; // duplicated table of anchors name
+
+      for (let i = 0; i < a_table.length; i++) {
+        $(".section4-" + a_table[i] + ".active .aos-init").addClass(
+          "aos-animate"
+        ); // all magic goes here - when page is active, then all elements with AOS will start animate
+      }
+    },
+
+    afterRender: function (index, anchorIndex) {
+      AOS.init(); // AOS initiation
+    },
+    afterLoad: function (index, anchorIndex) {
+      AOS.init(); // AOS initiation
+      $(".aos-init").removeClass("aos-animate");
+
+      const a_table = ["first", "second", "third", "forth"];
+
+      for (let i = 0; i < a_table.length; i++) {
+        $(".section4-" + a_table[i] + ".active .aos-init").addClass(
+          "aos-animate"
+        );
+      }
+    },
+    onLeave: function (origin, destination, direction) {},
+  });
+
+  //스크롤 다운
+  $(".scroll-down").on("click", function () {
+    $("#fullpage").fullpage.moveSectionDown();
+  });
+
+  $(document).on("click", '.nav a[href!="#"]', function (e) {
+    $(".ui-view").fullpage.destroy("all");
+    clearTimeoutAll();
+  });
+})(jQuery);
